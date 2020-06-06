d294cfbaf72658583dd5e20bd05901512dac0083
Thanh Nguyen Ngoc <thanhthanchuc@gmail.com>
2020-06-06 17:45:07 +0700

    feat: config router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import './App.css';
 
 import Home from 'screens/Home';
+import Dashboard from 'screens/Dashboard';
 
 require('dotenv').config();
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
