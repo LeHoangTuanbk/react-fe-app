30cbf842c8d142f0e700cf036713f14b72c7009a
thegreatman <lehoangtuanbk@gmail.com>
2020-06-30 16:05:21 +0700

    code Interface Okay

--- src/components/Activity/index.jsx
+++ src/components/Activity/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Space, Table } from 'antd'
 import formatDateTime from 'utils/formatDateTime'
+import moment from 'moment';
 
 export default class Activity extends React.PureComponent {
   render() {
@@ -16,7 +17,8 @@ export default class Activity extends React.PureComponent {
         title: 'Unlock Date',
         dataIndex: 'unclock_date',
         key: 'unclock_date',
-        render: ((text, record) => <span>{formatDateTime(record.unclock_date)}</span>)
+        render: ((text, record) => <span>{formatDateTime(record.unclock_date)}</span>),
+        sorter: (prevRecord, nextRecord) => moment(prevRecord.unclock_date).unix() - moment(nextRecord.unclock_date).unix()
       },
       {
         title: 'Tên đăng nhâp',
